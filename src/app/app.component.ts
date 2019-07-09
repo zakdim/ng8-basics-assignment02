67e70cfbc61685658b740b446669da470b2e03f2
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2019-07-09 00:21:25 -0400

    s02-basics assignment 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng8-basics-assignment02';
+  username: string = '';
+
+  isUsernameEmpty() {
+    return this.username.trim() === '';
+  }
 }
